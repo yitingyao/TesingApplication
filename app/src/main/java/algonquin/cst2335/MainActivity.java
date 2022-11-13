8b38e86b50a5361abcff1728cdd966efb68c18f4
Yiting Yao <yao00043@algonquinlive.com>
2022-11-13 17:55:50 -0500

    Commit to Week4

--- app/src/main/java/algonquin/cst2335/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/MainActivity.java
@@ -1,38 +1,72 @@
 package algonquin.cst2335;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.util.Log;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button loginButton;
+    EditText editEmail;
+    SharedPreferences prefs;
+    //SharedPreferences is telling the application that this is default and to save it, such that it's there you open it again
+    private static final String SHARED_PREF_NAME = "MyData";
+    private static final String KEY_LOGIN = "email";
+    //these are private final because they can only be accessed by this class and won't change
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
+        loginButton = findViewById(R.id.loginButton);
+        editEmail = findViewById(R.id.editEmail);
+        loginButton.setOnClickListener(click -> {
+            secondActivity();
+        });
+
+        prefs = getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
+        //getSharedPreferences is an android method that looks for the sharedpreferences with the name given in the parameters
+        //assigns mode to it, in this case private
+        //all our SharedPreferences is stored in Android, and right now we just want those two to be stored in prefs
+
+        String email = prefs.getString(KEY_LOGIN, null);
+        //inside prefs look for variable KEY_LOGIN which holds string value "email" in our case
+        //value of variable email, which is the email that user inputted and storing it in email
+
+
+        editEmail.setText(email);
+        //this sets the defaut login page each time you open to the email you had last time
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
+    public void secondActivity() {
+        //secondActivity always means another page
+        SharedPreferences.Editor editor = prefs.edit();
+        //declaring our editor
+        //the editor edits the shared preferencces
+        editor.putString(KEY_LOGIN, editEmail.getText().toString());
+        //getting the editEmail that was already set to the email user inputed
+        //editor.putString(yao@hotmail.com) that stores the email I put as a shared preference so that SecondActivity can access
+        //Then in SecondActivity we will get the value from our previous activity from our shared preferences
+        //only putString is related to SharedPreferencews
+        editor.apply();
+        //need to call apply to save putString
+        Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
+        //Intent is a box (in this case emails) delivering this to SecondAcitivity
+        nextPage.putExtra("EmailAddress", editEmail.getText().toString());
+        //nextPage is delivering the stored email into SecondActivity
+        startActivity(nextPage);
+        //secondActivity triggers nextPage intent
+        //purpose of this method is to switch from main activity to second activity
+        //purpose of secondActivity.class is to take phone number input from user, and call phone number
+        //we switch between activities to get to another app
+        //An intent allows you to start an activity in another app by describing a simple action you'd like to perform
+        //This intent says that you want to transition from MainActivity to SecondActivity. Then to actually make the transition, you call startActivity with the intent object:///
     }
 }
--- app/src/main/java/algonquin/cst2335/SecondActivity.java
+++ app/src/main/java/algonquin/cst2335/SecondActivity.java
@@ -1,21 +1,117 @@
 package algonquin.cst2335;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 
 public class SecondActivity extends AppCompatActivity {
+
+    private static final String SHARED_PREF_NAME = "MyData";
+    private static final String KEY_PHONE = "phone";
+    TextView textView;
+    EditText editTextPhone;
+    Button buttonCallNumber;
+    Button buttonChangePicture;
+    SharedPreferences prefs;
+    ImageView cameraImage;
+
+    ActivityResultLauncher<Intent> cameraResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+        //result holds what is coming back from the activity opened, in this case it would hold picture taken from camera
+        if (result.getResultCode() == Activity.RESULT_OK) {
+            //has to check if photo is taken or not
+            Intent data = result.getData();
+            //if photo is taken, getData will get photo
+            Bitmap thumbnail = data.getParcelableExtra("data");
+            //bitmap is android object that stores visuals
+            cameraImage.setImageBitmap(thumbnail);
+            //set the bitmap into imageview, the bitmap holds the visual
+            saveImage(thumbnail);
+        }
+    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        textView = findViewById(R.id.textView);
+        editTextPhone = findViewById(R.id.editTextPhone);
+        buttonCallNumber = findViewById(R.id.buttonCallNumber);
+        buttonChangePicture = findViewById(R.id.buttonChangePicture);
+        cameraImage = findViewById(R.id.cameraImage);
+        Intent fromPrevious = getIntent();
+        String emailAddress = fromPrevious.getStringExtra("EmailAddress");
+        textView.setText("Welcome back " + emailAddress);
+        prefs = getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
+        String phone = prefs.getString(KEY_PHONE, null);
+        editTextPhone.setText(phone);
+        buttonCallNumber.setOnClickListener(view -> {
+            savePhoneNumber();
+            Intent call = new Intent(Intent.ACTION_DIAL);
+            call.setData(Uri.parse("tel:" + editTextPhone.getText().toString()));
+            startActivity(call);
+        });
+
+        buttonChangePicture.setOnClickListener(view -> {
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+            cameraResult.launch(cameraIntent);
+
+        });
+        File file = new File(getFilesDir(), "Picture.png");
+
+        if (file.exists()) {
+            Bitmap theImage = BitmapFactory.decodeFile(file.getAbsolutePath());
+            //pulling the file, extracting the bitmap for the old saved image
+            cameraImage.setImageBitmap(theImage);
+
+        }
+
     }
 
+    private void savePhoneNumber() {
+        SharedPreferences.Editor editor = prefs.edit();
+        //declaring our editor
+        //the editor edits the shared preferencces
+        editor.putString(KEY_PHONE, editTextPhone.getText().toString());
+        //getting the editEmail that was already set to the email user inputed
+        //editor.putString(yao@hotmail.com) that stores the email I put as a shared preference so that SecondActivity can access
+        //Then in SecondActivity we will get the value from our previous activity from our shared preferences
+        //only putString is related to SharedPreferencews
+        editor.apply();
+    }
 
-
-
-
+    private void saveImage(Bitmap mBitmap) {
+        FileOutputStream fOut = null;
+        try {
+            fOut = openFileOutput("Picture.png", Context.MODE_PRIVATE);
+            mBitmap.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+            fOut.flush();
+            fOut.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
